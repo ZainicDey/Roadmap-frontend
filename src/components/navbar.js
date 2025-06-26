2057be5e4e266ca6bddc2d78b8bd50641c5d2b8b
Anik Dey <ihanik.ad@gmail.com>
2025-06-27 03:21:01 +0600

    smallest bug

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -61,7 +61,10 @@ export default function Navbar() {
             {isMobileMenuOpen && (
                 <div className="mobile-menu md:hidden px-4 pb-4">
                     <div className="flex flex-col space-y-1">
-                        <a href="/" className="py-2 px-0 text-sm text-gray-500 rounded hover:bg-gray-200 inline-block">
+                        <a
+                        onClick={() => window.location.reload()}
+                            className="py-2 px-0 text-sm text-gray-500 rounded hover:bg-gray-200 inline-block cursor-pointer"
+                            >
                             <span className="px-3">Refresh</span>
                         </a>
                         <a onClick={logout} className="py-2 px-0 text-sm text-red-500 rounded hover:bg-gray-200 inline-block">
